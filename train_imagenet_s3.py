375ae3d5e0f57b3c1f720d4d2e56d765fb971b05
Aqib Ahmad <aqib398@gmail.com>
2025-03-14 12:05:46 -0400

    Update train_imagenet_s3.py

--- train_imagenet_s3.py
+++ train_imagenet_s3.py
@@ -1,4 +1,3 @@
-import os
 import torch, torchvision
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -10,89 +9,58 @@ from PIL import UnidentifiedImageError
 from s3torchconnector import S3MapDataset, S3IterableDataset, S3ClientConfig, S3Checkpoint
 from functools import partial
 from tqdm import tqdm
-
-
-# S3 Configuration
-os.environ["AWS_ACCESS_KEY_ID"] = "YOUR_ACCESS_KEY"
-os.environ["AWS_SECRET_ACCESS_KEY"] = "YOUR_SECRET_KEY"
-S3_ENDPOINT = "YOUR_S3_ENDPOINT"
-throughput_target_gbps = 10.0
-part_size = 8 * 1024 * 1024 # => 8MiB
-TRAIN_DATASET_URI="s3://imagenet/train"
-VAL_DATASET_URI="s3://imagenet/val"
-CHECKPOINT_URI="s3://imagenet/checkpoints/"
-REGION = "us-east-1"
-
-# Train Settings
-num_epochs = 100
-batch_size = 256
-dataload_workers = 16
-
-# Required
-label_map = {}
-
-
-def transform_image(object, type):
-
-    debug_dict = {}
-
-    try:
-
-        debug_dict["state_before"] = vars(object)
-        img = Image.open(object)
-
-    except UnidentifiedImageError:
-
-        print("exception caught!")
-        debug_dict["state_after"] = vars(object)
-        print(object.key)
-        from pprint import pprint
-        pprint(debug_dict)
-        raise
-        
-
-    if img.mode != "RGB":
-        img = img.convert("RGB")
-
-    class_name = object.key.split("/")[1]
-    class_idx = label_map[class_name]
-
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-
-    transforms_to_apply = None
-    if type == "train":
-        transforms_to_apply = transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize
-        ])
-    elif type == "val":
-        transforms_to_apply = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize
-        ])
-
-    img = transforms_to_apply(img)
-
-    return (img, class_idx)
-
+import os
+from utils import transform_image
 
 
 
 if __name__ == '__main__':
 
-    # Define your execution device
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-    print("[INFO] The model will be running on", device, "device")
+
+    # S3 Configuration
+    os.environ["AWS_ACCESS_KEY_ID"] = "testuser"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testuser"
+    throughput_target_gbps = 10.0
+    part_size = 8 * 1024 * 1024 # => 8MiB
+    S3_ENDPOINT = "http://cxo-s3-cluster47.lab.nimblestorage.com"
+    TRAIN_DATASET_URI="s3://imagenet/train/"
+    VAL_DATASET_URI="s3://imagenet/val/"
+    CHECKPOINT_URI="s3://imagenet/checkpoints/"
+    REGION = "us-east-1"
+
+    # Train Settings
+    num_epochs = 100
+    batch_size = 256
+    dataload_workers = 16
+
+
+    # Global Vars
+    label_map = {}
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225])
+    train_transforms = transforms.Compose([
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    normalize
+                ])
+    val_transforms = transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize
+                ])
+
+
+
+
+
 
 
     s3_config = S3ClientConfig(throughput_target_gbps=throughput_target_gbps, part_size=part_size, force_path_style=False)
-    train_dataset = S3MapDataset.from_prefix(TRAIN_DATASET_URI, region=REGION,endpoint=S3_ENDPOINT, s3client_config=s3_config, transform=partial(transform_image, type="train"))
-    val_dataset = S3MapDataset.from_prefix(VAL_DATASET_URI, region=REGION,endpoint=S3_ENDPOINT, s3client_config=s3_config, transform=partial(transform_image, type="val"))
+    train_dataset = S3MapDataset.from_prefix(TRAIN_DATASET_URI, region=REGION,endpoint=S3_ENDPOINT, s3client_config=s3_config, transform=partial(transform_image, type="train", train_transforms=train_transforms, val_transforms=val_transforms, label_map=label_map))
+    val_dataset = S3MapDataset.from_prefix(VAL_DATASET_URI, region=REGION,endpoint=S3_ENDPOINT, s3client_config=s3_config, transform=partial(transform_image, type="val", train_transforms=train_transforms, val_transforms=val_transforms, label_map=label_map))
     checkpoint = S3Checkpoint(region=REGION, endpoint=S3_ENDPOINT, s3client_config=s3_config)
 
 
@@ -105,9 +73,15 @@ if __name__ == '__main__':
         label_map[label] = i
     print("Total number of classes: ", str(len(label_map)))
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=dataload_workers, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=dataload_workers, shuffle=False) 
 
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=dataload_workers, pin_memory=True, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=dataload_workers, pin_memory=True, shuffle=False) 
+
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+    print("[INFO] The model will be running on", device, "device")
 
     best_accuracy = 0.0
     # Choose ResNet-18 if GPU is present otherwise MobileNet_V3
@@ -118,6 +92,8 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.SGD(model.parameters(), 0.1, momentum=0.9, weight_decay=1e-4)
     scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
+
+
 
     print("[INFO] Starting model training.")
     # Training loop
@@ -183,6 +159,7 @@ if __name__ == '__main__':
                     torch.save(model.state_dict(), writer)
 
                 best_accuracy = accuracy
+
 
 
 
